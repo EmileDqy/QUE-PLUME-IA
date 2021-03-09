ae4a2e90c05e045a99f16c2b28517f9059db7cc9
Emile <emile.duquennoy@gmail.com>
2021-03-09 10:11:24 +0100

    Add files via upload

---
+++ src/manager.h
@@ -0,0 +1,10 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <Calibration/CameraCalibration>
+#include <Tracker_Object_Recognition/TrackerRecognition>
+#include <main.cpp>
+
+void trackAndRecognize();
+
+#endif
