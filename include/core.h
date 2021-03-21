9e95c6fe0e43f146aae8d867407f32702723cb0a
AlioptK <Hupode)6>
2021-03-21 21:47:29 +0100

    Updating core.h

--- include/core.h
+++ include/core.h
@@ -1,8 +1,12 @@
 #ifndef HEADER_FILE_H
 #define HEADER_FILE_H
 
+#include <vector>
+#include <opencv2/core/utility.hpp>
+
 void calibrate();
 void trackAndRecognize();
+std::vector<int> getColor(cv::Mat frame1);
 
 #endif // HEADER_FILE_H
 
