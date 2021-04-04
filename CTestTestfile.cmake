2c41f05a74db98450d486fffecea9fdf81f29b7c
Emile Duquennoy <emile.duquennoy@hei.yncrea.fr>
2021-04-04 15:04:09 +0200

    Removing CMake files from remote

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/QUE-PLUME-IA
-# Build directory: /home/pi/QUE-PLUME-IA
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
